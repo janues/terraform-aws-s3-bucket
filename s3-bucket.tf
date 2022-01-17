77ba640edd40266aacfb87ffdd62b71b27fef99c
janues <janues@naver.com>
2022-01-17 06:14:28 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "my-s3-bucket"
-  bucket_prefix = "prefix"
+  bucket_prefix = "jhjeon"
   acl    = "private"
 
   versioning = {
